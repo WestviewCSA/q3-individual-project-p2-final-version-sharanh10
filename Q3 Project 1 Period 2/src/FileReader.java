398adace1865a157fb5be2493268115389886b51
1944695 <1944695@01-18109.pusd.dom>
2026-02-23 11:49:51 -0800

    2/23 maze commmits

---
+++ Q3 Project 1 Period 2/src/FileReader.java
@@ -0,0 +1,87 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class FileReader {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Queue q = getText("easyMap1");
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());
+		}
+		System.out.println("");
+		String[][] n = getTextCoords("easyMapCoords");
+		for(int i = 0; i<n.length;i++) {
+			for(int j = 0; j<n[0].length; j++) {
+				System.out.print(n[i][j]);
+			}
+			System.out.println("");
+		}
+
+	}
+	
+	public static Queue<String> getText(String passedFile) {
+		Queue<String> mapVals = new ArrayDeque<>();
+		File file = new File(passedFile);
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			String rows = scan.next();
+			String cols = scan.next();
+			String levels = scan.next();
+			while(scan.hasNext()) {
+				//String nextval = scan.next();
+				mapVals.add(scan.next());
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return mapVals;
+
+	}
+	
+	public static String[][] getTextCoords(String passedFile) {
+		File file = new File(passedFile);
+		
+		Scanner scan;
+		try {
+			scan = new Scanner(file);
+			String rows = scan.next();
+			String cols = scan.next();
+			String[][] coords = new String[Integer.parseInt(rows)][Integer.parseInt(cols)];
+			
+			
+			String levels = scan.next();
+			while(scan.hasNext()) {
+				String num = scan.next();
+				rows = scan.next();
+				cols = scan.next();
+				scan.next();
+				//String nextval = scan.next();
+				coords[Integer.parseInt(rows)][Integer.parseInt(cols)] = num;
+			}
+			for(int i = 0; i<coords.length;i++) {
+				for(int j = 0; j<coords[0].length; j++) {
+					if(coords[i][j]==null) {
+						coords[i][j] = ".";
+					}
+				}
+			}
+			return coords;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		
+
+	}
+
+
+}
+
